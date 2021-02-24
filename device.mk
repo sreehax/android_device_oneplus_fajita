10f34a4a8887f990b93dd501bae66d202ea1ae4b
Sreehari S <sreeharisreedev1@gmail.com>
2021-02-23 16:57:40 -0800

    attempt to get rid of FOD

--- device.mk
+++ device.mk
@@ -30,8 +30,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
-PRODUCT_COPY_FILES += \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+#PRODUCT_COPY_FILES += \
+#    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -42,10 +42,10 @@ TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_fajita \
-    vendor.oneplus.fingerprint.extension@1.0 \
-    vendor.oneplus.hardware.display@1.0
+#PRODUCT_PACKAGES += \
+#    lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_fajita \
+#    vendor.oneplus.fingerprint.extension@1.0 \
+#    vendor.oneplus.hardware.display@1.0
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
